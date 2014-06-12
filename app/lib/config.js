d1e42b095a8251ed5efb8f2ea9117fada88b2637
Jordan Pellizzari <jpellizzari21@gmail.com>
2014-06-11 21:53:16 -0700

    findPlayerByName method, config.js added

---
+++ app/lib/config.js
@@ -0,0 +1,14 @@
+//Configuration objects that can be called into various functions
+
+exports.dev_db = {
+  host     : 'localhost',
+  user     : 'root',
+  database : 'dayz_epoch'
+}
+
+exports.prod_db = {
+  host     : 'localhost',
+  user     : 'dayz',
+  password : 'M0n!caL3w!nsky',
+  database : 'dayz_epoch'
+}
