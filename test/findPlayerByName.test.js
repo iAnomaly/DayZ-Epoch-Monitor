e836ab04a4f63c7291fc6f33d9c22e4946a9f3d6
Jordan Pellizzari <jpellizzari21@gmail.com>
2014-06-12 19:11:40 -0700

    added mocha to package.json

--- test/findPlayerByName.test.js
+++ test/findPlayerByName.test.js
@@ -2,7 +2,7 @@
 var assert = require('assert')
 var db = require('../app/lib/db')
 
-//This test passes even though it should not pass. Needs to be fixed.
+
 describe('db.findPlayerByName', function (){
 	it('returns a player named Friache', function (done){
 		db.findPlayerByName('Friache', function (data){
