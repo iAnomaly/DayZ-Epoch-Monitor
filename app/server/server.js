54bdf44a52948064bbdf842bfe232d77c73f9661
Jordan Pellizzari <jpellizzari21@gmail.com>
2014-06-15 23:54:29 -0700

    added /api routes and integration tests

--- app/server/server.js
+++ app/server/server.js
@@ -1,16 +1,34 @@
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app);
+    db = require('../../lib/db')
+
+//Export for integration testing
+exports.app = app;
 
 // Express
 app.use(express.static(__dirname + '/../client/'));
+app.use(express.logger('dev'));
 
 // Client
 app.get('/', function(req, res) {
     res.sendfile('../client/index.html');
 });
 
-// API
-//app.all('/api/', blarg);
 
 server.listen(process.argv[2] || 1337);
+
+// API
+//app.all('/api/', blarg);
+app.get('/api/players', function (req, res){
+	db.getAllPlayers(function (data){
+		res.send(data);
+	})
+})
+
+app.get('/api/players/:name', function (req, res){
+	var player_name = req.param('name')
+	db.getPlayerByName(player_name, function (data){
+		res.send(data)
+	})
+})
