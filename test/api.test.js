14341c2d3d7c3c4bde40016398b4cb4e1cfcdd68
Jordan Pellizzari <jpellizzari21@gmail.com>
2014-06-16 20:12:58 -0700

    added api integration tests

---
+++ test/api.test.js
@@ -0,0 +1,23 @@
+var request = require('supertest')
+var server = require('../app/server/server.js')
+
+describe('Api Routes', function (){
+	describe('/api/players', function (){
+		it('responds with json', function (done){
+			request(server.app)
+				.get('/api/players')
+				.set('Accept', 'application/json')
+				.expect('Content-type', /json/)
+				.expect(200, done);
+		})
+	}),
+	describe('/api/players/:name', function (){
+		it('responds with json', function (done){
+			request(server.app)
+				.get('/api/players/Friache')
+				.set('Accept', 'application/json')
+				.expect('Content-type', /json/)
+				.expect(200, done)
+		})
+	})
+})
