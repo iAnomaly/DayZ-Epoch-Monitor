ef2cbe6710151caccfdd427f226b4dfa5aef10ea
Jordan Pellizzari <jpellizzari21@gmail.com>
2014-06-23 22:43:24 -0700

    initial DiffEngine object created

---
+++ test/history.test.js
@@ -0,0 +1,16 @@
+//Unit tests for DiffEngine
+
+var assert = require('assert');
+var diffEngine = require('../lib/diff');
+var fixtures = require('./fixtures')
+
+var diff = new diffEngine();
+
+
+describe('DiffEngine', function (){
+	it('returns the diff of two DBClient results', function (done){
+		diff.consume(fixtures.first_poll);
+		diff.consume(fixtures.second_poll);
+		done();
+	});
+});
